4c3c48bfba5f33f66ddf405e934952bb2bcb9450
unknown <cristianodasilva.azevedo@gmail.com>
2016-10-29 15:47:05 -0200

    Exercício com angular e testes

--- test/index/IndexService-spec.js
+++ test/index/IndexService-spec.js
@@ -9,7 +9,8 @@ describe('IndexService', function () {
     it('Inserir um array e verificar seu length', function () {
         var berr = { nome: 'Skol', teor: '10' };
         service.insert(berr);
-        expect(service.getAll().length).toEqual(1)
+        expect(service.getAll().length).toEqual(1);
+        expect(service.getAll()[0].nome).toEqual(berr.nome);
     });
 
     it('Remover um elemento do array e verificar seu length', function () {
@@ -19,5 +20,6 @@ describe('IndexService', function () {
         service.insert(brahma);
         service.remove(0);
         expect(service.getAll().length).toEqual(1)
+        expect(service.getAll()[0].nome).toEqual(brahma.nome);
     });
 });
